b574ff05350baf2f108ce988e02be5014da3eb89
mariiwee <mariiweei@gmail.com>
2023-01-18 20:43:25 +0600

    added todo closed

--- main/views.py
+++ main/views.py
@@ -24,6 +24,12 @@ def delete_todo(request, id):
 
 def mark_todo(request, id):
     todo = ToDo.objects.get(id=id)
-    todo.is_favorite = True
+    todo.is_favorite = not todo.is_favorite
+    todo.save()
+    return redirect(test)
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
     todo.save()
     return redirect(test)
